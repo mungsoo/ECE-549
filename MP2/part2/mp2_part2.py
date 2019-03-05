62aaa97ef009384d55c2fdcc197e3343ef814932
mungsoo <346582379@qq.com>
2019-03-05 14:19:39 -0600

    mp2 done

--- MP2/part2/mp2_part2.py
+++ MP2/part2/mp2_part2.py
@@ -27,7 +27,14 @@ def show_all_circles(image, cx, cy, rad, color='r'):
 
 
 def get_scale_space(input, num_sigma, min_sigma, sigma_ratio, octave_size, method="downsample"):
-
+    """
+    input: numpy array, input image
+    num_sigma: int, number of different sigma to compute
+    min_sigma: float, the initial sigma, the subsequent sigma will be computed by multipling a constant together
+    sigma_ratio: float, the constant ratio of consecutive sigma
+    octave_size: int, only valid when method is "dog", size of each octave_size
+    method: string, one of "normal", "downsample", "dog"
+    """
     x_dim = input.shape[0]
     y_dim = input.shape[1]
     scale_space = np.empty((x_dim, y_dim, num_sigma))
@@ -51,24 +58,29 @@ def get_scale_space(input, num_sigma, min_sigma, sigma_ratio, octave_size, metho
                 scale_space[:, :, i] = skimage.transform.resize(intermidiate, (x_dim, y_dim), anti_aliasing=False)
             else:
                 scale_space[:, :, i] = intermidiate
+
     elif method == "dog":   
+        # Naive approach, slow
+        #
         # sigma_list = np.array([min_sigma * (sigma_ratio ** i)
                            # for i in range(num_sigma + 1)])
         # gaussian_images = [gaussian_filter(input, s) for s in sigma_list]
         # for i in range(num_sigma):
-            # scale_space[:, :, i] = (gaussian_images[i] - gaussian_images[i+1]) * sigma_list[i]
+            # scale_space[:, :, i] = (gaussian_images[i] - gaussian_images[i+1]) ** 2
     
+        # A slightly faster approach, do not downsample for every octave
+        #
         # cur_sigma = min_sigma
         # gaussian_image = input
-        # cur_gaussian = gaussian_filter(gaussian_image, cur_sigma)
+        # cur_gaussian = gaussian_filter(gaussian_image, cur_sigma, mode="nearest")
         # pre_gaussian = None
         
         # for i in range(num_sigma):
             # pre_gaussian = cur_gaussian
-            # cur_sigma = (sigma_ratio - 1) * cur_sigma
-            # cur_gaussian = gaussian_filter(pre_gaussian, cur_sigma)
-            # scale_space[:, :, i] = (pre_gaussian - cur_gaussian)
-            # cur_sigma = cur_sigma * sigma_ratio / (sigma_ratio - 1)
+            # cur_sigma = np.sqrt(sigma_ratio - 1) * cur_sigma
+            # cur_gaussian = gaussian_filter(pre_gaussian, cur_sigma, mode="nearest")
+            # scale_space[:, :, i] = (pre_gaussian - cur_gaussian) ** 2
+            # cur_sigma = cur_sigma * sigma_ratio / np.sqrt(sigma_ratio - 1)
     
     
         sigma_ratio = np.power(2, (1.0 / octave_size))
@@ -76,19 +88,19 @@ def get_scale_space(input, num_sigma, min_sigma, sigma_ratio, octave_size, metho
         cur_sigma = min_sigma
         cur_gaussian = gaussian_filter(gaussian_image, cur_sigma)
         pre_gaussian = None
+   
         for i in range(num_sigma):
             if i != 0 and i % octave_size == 0:
                 gaussian_image = skimage.transform.resize(gaussian_image, (gaussian_image.shape[0] \
-                                        // 2, gaussian_image.shape[1] // 2), anti_aliasing=True)
+                                        // 2, gaussian_image.shape[1] // 2), anti_aliasing=False)
                 cur_sigma = min_sigma
                 cur_gaussian = gaussian_filter(gaussian_image, cur_sigma)
                 
             pre_gaussian = cur_gaussian
             cur_sigma = sigma_ratio * cur_sigma
             cur_gaussian = gaussian_filter(gaussian_image, cur_sigma)
-            scale_space[:, :, i] = skimage.transform.resize(cur_gaussian - pre_gaussian, input.shape, anti_aliasing=False)
-        # print("gogodsfdassssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss")
-
+            scale_space[:, :, i] = skimage.transform.resize(cur_gaussian - pre_gaussian, input.shape, anti_aliasing=False) ** 2
+            
     else:
         print("Error in method: No %s" % method)
 
@@ -96,7 +108,14 @@ def get_scale_space(input, num_sigma, min_sigma, sigma_ratio, octave_size, metho
         
         
 def non_maximum_suppression(scale_space, num_sigma, threshold, nms_size, exclude_border):
-
+    """
+    scale_space: numpy array
+    num_sigma: int, number of different sigma to compute
+    threshold: float, the maxima under threshold will be wiped out
+    nms_size: int, the size will be used to call rank_filter
+    sigma_ratio: float, the constant ratio of consecutive sigma
+    exclude_border: int, the locations of maxima whose distance is under exclude_border will be wiped out
+    """
     # Get the local maxima in the across all scales with size(nms_size, nms_size)
     local_max = rank_filter(scale_space, rank=-1, size=(nms_size, nms_size, num_sigma), mode="constant")
     local_max[local_max != scale_space] = 0
@@ -113,7 +132,17 @@ def non_maximum_suppression(scale_space, num_sigma, threshold, nms_size, exclude
     
 def blob_detection(input, num_sigma=10, min_sigma=2, sigma_ratio=1.2599, octave_size=3, method="downsample",\
                    threshold=0.01, nms_size=10, exclude_border=10):
-    
+    """
+    input: numpy array, input image
+    num_sigma: int, number of different sigma to compute
+    min_sigma: float, the initial sigma, the subsequent sigma will be computed by multipling a constant together
+    sigma_ratio: float, the constant ratio of consecutive sigma
+    octave_size: int, only valid when method is "dog", size of each octave_size
+    method: string, one of "normal", "downsample", "dog"
+    threshold: float, the maxima under threshold will be wiped out
+    nms_size: int, the size will be used to call rank_filter
+    exclude_border: int, the locations of maxima whose distance is under exclude_border will be wiped out
+    """
     print("<--------------------Blob Detection-------------------->")
     start = time.time_ns()
     scale_space = get_scale_space(input, num_sigma, min_sigma, sigma_ratio, octave_size, method=method)
@@ -130,16 +159,11 @@ def blob_detection(input, num_sigma=10, min_sigma=2, sigma_ratio=1.2599, octave_
     center = nonzero_element[:2]
     show_all_circles(input, center[1], center[0], radius)
 
-
-k = 1.2599
-num_sigma = 10
-min_sigma = 2
-threshold = 0.02
-nms_size = 3
-butterfly = cv2.imread("butterfly.jpg", 0).astype("float") / 255    
-blob_detection(butterfly, num_sigma, method="dog", threshold=threshold, nms_size=nms_size)
-
-# aa = gaussian_filter(gaussian_filter(butterfly, 2), (1.259 - 1) * 2)
-# bb = gaussian_filter(butterfly, 2 * 1.259)
-# print(aa-bb)
-# print(aa)
+if __name__ == "__main__":
+    k = 1.2599
+    num_sigma = 10
+    min_sigma = 2
+    threshold = 0.02
+    nms_size = 10
+    butterfly = cv2.imread("butterfly.jpg", 0).astype("float") / 255    
+    blob_detection(butterfly, num_sigma, method="normal", threshold=threshold, nms_size=nms_size)
